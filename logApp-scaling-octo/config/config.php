c37d9cf3e629f037a78fcfe317a0462d851caa95
Arnel1001 <202080475@psu.palawan.edu.ph>
2022-10-12 02:31:11 +0800

    config.php modified

--- logApp-scaling-octo/config/config.php
+++ logApp-scaling-octo/config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', '');
-	define('DB_HOST', '');
-	define('DB_USER', '');
-	define('DB_PASS', '');
-	define('DB_NAME', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525831');
+	define('DB_PASS', 'Pnegt7zXw1');
+	define('DB_NAME', 'sql6525831');
