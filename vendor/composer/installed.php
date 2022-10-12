5e385fd0073a249dbe3e58402ee6c448341f7a7f
Arnel1001 <202080475@psu.palawan.edu.ph>
2022-10-12 14:15:27 +0800

    changes made

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '23263d28d2ca2a90c4207f82f01c575fdac7ef9c',
+        'name' => 'arnel1001/logapp-llabores',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'arnel1001/logapp-llabores' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '23263d28d2ca2a90c4207f82f01c575fdac7ef9c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
